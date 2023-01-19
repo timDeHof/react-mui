7a39c35b9aa2cd7757875d263a8f7bb25be31ecf
timDeHof <this.is.tim.dehof@gmail.com>
2023-01-19 16:27:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Settings, Add } from "@mui/icons-material"
+import { Button, Typography, styled } from "@mui/material"
 
 function App() {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "#fff",
+    },
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant='text'>Text</Button>
+      <Button startIcon={<Settings />} variant='contained' color='secondary' size='small'>
+        Contained
+      </Button>
+      <Button startIcon={<Add />} variant='contained' color='success' size='small'>
+        Contained
+      </Button>
+      <Button variant='outlined' disabled>
+        Outlined
+      </Button>
+      <Typography variant='h1' component='p'>
+        It uses h1 style but it's a p tag
+      </Typography>
+      <BlueButton>My Unique Button</BlueButton>
+      <BlueButton>Another Button</BlueButton>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
